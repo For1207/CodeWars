b5d5e9ce8906f1fc613c7af4780d704ae5b1e786
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-02-03 23:06:29 -0500

    Final commit

--- codeWars.js
+++ codeWars.js
@@ -3,10 +3,9 @@ function accum(s) {
   let newArr = [];
   for (i=0; i<splitedS.length; i++) {
     newArr[i] = splitedS[i].toUpperCase(splitedS[i]) + splitedS[i].repeat(i)  + '-';
-
-    newArr.push(newArr[i]);
-
   }
+  newArr = newArr.join("");
+  newArr = newArr.slice(0, newArr.length - 1);
   console.log(newArr);
 }
 
