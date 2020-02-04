dc613e2bf3454115c1ca803f31b87411d63b7b5a
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-02-03 22:40:34 -0500

    Feature: Convert to a separated string
    In addtion:
    - convert to lower case;
    - capitalize the first letter;
    - add dash at the end of every elements
    Bug: there is a dash at the very end

--- codeWars.js
+++ codeWars.js
@@ -1,5 +1,13 @@
 function accum(s) {
-	// your code
+	let splitedS = s.toLowerCase(s.split(""));
+  let newArr = [];
+  for (i=0; i<splitedS.length; i++) {
+    newArr[i] = splitedS[i].toUpperCase(splitedS[i]) + splitedS[i].repeat(i)  + '-';
+
+    newArr.push(newArr[i]);
+
+  }
+  console.log(newArr);
 }
 
 accum("RqaEzty");
