46a79d21a865c04ba3d05bce0b110e6a55db9396
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-02-26 18:09:47 -0500

    Readme added to "Mumbling"

--- 7kyu/Highest and Lowest/Highest_And_Lowest.js
+++ 7kyu/Highest and Lowest/Highest_And_Lowest.js
@@ -1,5 +1,14 @@
 function highAndLow(numbers){
+	let numArr = [];
+	let noSpacesArr = [];
+	numArr = numbers.split("");
+	for (i=0; i<numArr.length; i++) {
+		if (numArr[i] != " ") {
+			noSpacesArr.push(numArr[i]);
+			console.log(noSpacesArr);
+		}
 
+	}
 }
 
 highAndLow("1 2 3 4 5 -1 -2 -3 -4 -5");
