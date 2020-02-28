8d606599e98ce409e427069f0f3d593f551d6b05
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-02-27 20:46:11 -0500

    Solution of this kata added

--- 7kyu/Highest and Lowest/Highest_And_Lowest.js
+++ 7kyu/Highest and Lowest/Highest_And_Lowest.js
@@ -1,14 +1,25 @@
 function highAndLow(numbers){
 	let numArr = [];
+	let numArr1 = numbers.match(/[0-9]/g);
+	for (i=0; i<numArr1.length; i++) {
+	numArr1 = parseInt(numArr1, 10);
+	console.log(numArr1);
+}
 	let noSpacesArr = [];
-	numArr = numbers.split("");
-	for (i=0; i<numArr.length; i++) {
-		if (numArr[i] != " ") {
-			noSpacesArr.push(numArr[i]);
-			console.log(noSpacesArr);
-		}
-
-	}
+	// numArr = numbers.split("");
+	// for (i=0; i<numArr.length; i++) {
+	// 	if (numArr[i] != " ") {
+	// 		noSpacesArr.push(numArr[i]);
+	// 		// console.log(noSpacesArr);
+	//
+	// 	}
+	//
+	// }
 }
 
 highAndLow("1 2 3 4 5 -1 -2 -3 -4 -5");
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
--- 7kyu/Highest and Lowest/Readme.md
+++ 7kyu/Highest and Lowest/Readme.md
@@ -1,11 +1,12 @@
-In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+<b>In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.</b>
 
-Example:
+<b>Example:</b>
 
 highAndLow("1 2 3 4 5");  // return "5 1"
 highAndLow("1 2 -3 4 5"); // return "5 -3"
 highAndLow("1 9 3 4 -5"); // return "9 -5"
-Notes:
+
+<b>Notes:</b>
 
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
