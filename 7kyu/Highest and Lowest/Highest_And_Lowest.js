6444a38cf7dbcbfa201711274f851f7a21f55305
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-02-27 20:57:10 -0500

    feat: String splited and converted to numbers

--- 7kyu/Highest and Lowest/Highest_And_Lowest.js
+++ 7kyu/Highest and Lowest/Highest_And_Lowest.js
@@ -1,20 +1,6 @@
 function highAndLow(numbers){
-	let numArr = [];
-	let numArr1 = numbers.match(/[0-9]/g);
-	for (i=0; i<numArr1.length; i++) {
-	numArr1 = parseInt(numArr1, 10);
-	console.log(numArr1);
-}
-	let noSpacesArr = [];
-	// numArr = numbers.split("");
-	// for (i=0; i<numArr.length; i++) {
-	// 	if (numArr[i] != " ") {
-	// 		noSpacesArr.push(numArr[i]);
-	// 		// console.log(noSpacesArr);
-	//
-	// 	}
-	//
-	// }
+let numArr = numbers.split(' ').map(Number);
+console.log(numArr);
 }
 
 highAndLow("1 2 3 4 5 -1 -2 -3 -4 -5");
@@ -23,3 +9,5 @@ function highAndLow(numbers){
   numbers = numbers.split(' ').map(Number);
   return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
 }
+
+highAndLow("1 2 3 4 5 -1 -2 -3 -4 -5");
