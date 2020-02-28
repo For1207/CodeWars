5dd1b3fe0c4b29b1a788a4ae6e5559e686f63930
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-02-28 18:18:41 -0500

    feat: choose min and max number

--- 7kyu/Highest and Lowest/Highest_And_Lowest.js
+++ 7kyu/Highest and Lowest/Highest_And_Lowest.js
@@ -1,11 +1,4 @@
 function highAndLow(numbers){
-let numArr = numbers.split(' ').map(Number);
-console.log(numArr);
-}
-
-highAndLow("1 2 3 4 5 -1 -2 -3 -4 -5");
-
-function highAndLow(numbers){
   numbers = numbers.split(' ').map(Number);
   return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
 }
