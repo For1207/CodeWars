18a51d7b721d62a74c6dea409b1b38b5d3a02b53
Igor Mykhalchuk <mykhalchuk.i.a@gmail.com>
2020-03-01 20:29:01 -0500

    final commit

--- 7kyu/Get the Middle Character/Get_the_Middle_Character.js
+++ 7kyu/Get the Middle Character/Get_the_Middle_Character.js
@@ -1,4 +1,9 @@
-function getMiddle(s)
-{
-  //Code goes here!
+function getMiddle(s){
+  let result = 0;
+  if ((s.length % 2) == 0 ) {
+    result = s.charAt((s.length / 2) -1) + s.charAt(s.length / 2);
+  } else {
+    result = s.charAt(Math.floor(s.length/2));
+  }
+  return result;
 }
